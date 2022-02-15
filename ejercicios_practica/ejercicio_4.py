1aaee456f35da16ad02016f7afdc930e4a437785
Giagnacovo Ivan <93552640+giagnai@users.noreply.github.com>
2022-02-15 09:42:25 -0300

    Add files via upload

--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -31,7 +31,8 @@ if __name__ == '__main__':
     # para aplicar en este caso.
     list_numeros_str = ['5', '2', '3', '', '7', 'NaN']
         
-    list_numeros_int = [print(x) if x.isdigit() else print(0) for x in list_numeros_str]
+    list_numeros_int = [int(x) if x.isdigit() else 0 for x in list_numeros_str]
+    print(list_numeros_int)
 
     # ¿Ya terminaron el ejercicio? ¿Por qué no prueban
     # hacer negativo alguno de los números de la lista?
